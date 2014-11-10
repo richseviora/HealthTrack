cee9042698af0a89ca9df2d20704efe03ca3dfbe
Rich Seviora <richard.seviora@gmail.com>
2014-11-10 12:03:08 -0500

    Implemented Create User Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,10 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :date_of_birth, :gender, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  validates_presence_of :email, :name, :password_digest
-  validates_numericality_of :date_of_birth
+  validates_presence_of :email, :name, :password_digest, :date_of_birth
   has_secure_password
 end
