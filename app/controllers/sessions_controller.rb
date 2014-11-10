e358741daeef852b6b1db9ee9dff71c9ed7f587e
Rich Seviora <richard.seviora@gmail.com>
2014-11-10 13:37:31 -0500

    Implemented Sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+   @user = User.find_by_email(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: 'User Logged In'
+    else
+      flash.now[:error] = 'Invalid Username/Password'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_session_path, notice: 'Logged Out'
+  end
+end
