acf96ccc0852be2c8927286332e911f4b70bacfe
Rich Seviora <richard.seviora@gmail.com>
2014-11-10 11:25:15 -0500

    Create User Controller and Views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      flash.now[:error] = 'User could not be created.'
+      render :new
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   validates_presence_of :email, :name, :password_digest
   validates_numericality_of :date_of_birth
+  has_secure_password
 end
